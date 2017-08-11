9db9cad28f7932c548236a32d0f731cbc934d398
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2017-08-12 00:03:44 +0530

    Source Code added

---
+++ LifestyleStore/header.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-inverse navabar-fixed-top">
+               <div class="container">
+                   <div class="navbar-header">
+                       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                       </button>
+                       <a href="index.php" class="navbar-brand">Lifestyle Store</a>
+                   </div>
+                   
+                   <div class="collapse navbar-collapse" id="myNavbar">
+                       <ul class="nav navbar-nav navbar-right">
+                           <?php
+                           if(isset($_SESSION['email'])){
+                           ?>
+                           <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+                           <li><a href="settings.php"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
+                           <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                           <?php
+                           }else{
+                            ?>
+                            <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                           <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                           <?php
+                           }
+                           ?>
+                           
+                       </ul>
+                   </div>
+               </div>
+</nav>
---
+++ LifestyleStore/index.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Lifestyle Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+           <?php
+            require 'header.php';
+           ?>
+           <div id="bannerImage">
+               <div class="container">
+                   <center>
+                   <div id="bannerContent">
+                       <h1>We sell lifestyle.</h1>
+                       <p>Flat 40% OFF on all premium brands.</p>
+                       <a href="products.php" class="btn btn-danger">Shop Now</a>
+                   </div>
+                   </center>
+               </div>
+           </div>
+           <div class="container">
+               <div class="row">
+                   <div class="col-xs-4">
+                       <div  class="thumbnail">
+                           <a href="products.php">
+                                <img src="img/camera.jpg" alt="Camera">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                         <h3>Cameras</h3>
+                                         <p>Choose among the best available in the world.</p>
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-4">
+                       <div class="thumbnail">
+                           <a href="products.php">
+                               <img src="img/watch.jpg" alt="Watch">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                    <h3>Watches</h3>
+                                    <p>Original watches from the best brands.</p>
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-4">
+                       <div class="thumbnail">
+                           <a href="products.php">
+                               <img src="img/shirt.jpg" alt="Shirt">
+                           </a>
+                           <center>
+                               <div class="caption">
+                                   <h3>Shirts</h3>
+                                   <p>Our exquisite collection of shirts.</p>
+                               </div>
+                           </center>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <footer>
+               <div class="container">
+               <center>
+                   <p>Copyright &copy Lifestyle Store. All Rights Reserved. | Contact Us: +91 90000 00000</p>
+                   <p>This website is developed by Sajal Agrawal</p>
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
