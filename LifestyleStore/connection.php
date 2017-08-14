5794187ae0bed8f10a557e55e14f2173b5e9e9e6
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2017-08-15 00:44:49 +0530

    Create connection.php

---
+++ LifestyleStore/connection.php
@@ -0,0 +1,4 @@
+<?php
+$con=mysqli_connect("localhost","id2538044_sajalagrawal","******","id2538044_store") or die(mysqli_error($con));
+//$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
+?>
