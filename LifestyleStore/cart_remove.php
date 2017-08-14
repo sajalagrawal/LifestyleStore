3e05baf9576d6cb62badda5bcf523143845b15e8
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2017-08-15 00:40:42 +0530

    Delete cart_remove.php

--- LifestyleStore/cart_remove.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
-    session_start();
-    $item_id=$_GET['id'];
-    $user_id=$_SESSION['id'];
-    $delete_query="delete from users_items where user_id='$user_id' and item_id='$item_id'";
-    $delete_query_result=mysqli_query($con,$delete_query) or die(mysqli_error($con));
-    header('location: cart.php');
-?>
