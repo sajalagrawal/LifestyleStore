3996ec3d6947b015919f71b35cc25a168311e4a0
Andrew <oldm8reggie@gmail.com>
2023-08-30 15:07:38 +0200

    old to new mysqli removal

--- LifestyleStore/login_submit.php
+++ LifestyleStore/login_submit.php
@@ -1,40 +1,44 @@
 <?php
-    require 'connection.php';
-    session_start();
-    $email=mysqli_real_escape_string($con,$_POST['email']);
-    $regex_email="/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$/";
-    if(!preg_match($regex_email,$email)){
-        echo "Incorrect email. Redirecting you back to login page...";
-        ?>
-        <meta http-equiv="refresh" content="2;url=login.php" />
-        <?php
-    }
-    $password=md5(md5(mysqli_real_escape_string($con,$_POST['password'])));
-    if(strlen($password)<6){
-        echo "Password should have atleast 6 characters. Redirecting you back to login page...";
-        ?>
-        <meta http-equiv="refresh" content="2;url=login.php" />
-        <?php
-    }
-    $user_authentication_query="select id,email from users where email='$email' and password='$password'";
-    $user_authentication_result=mysqli_query($con,$user_authentication_query) or die(mysqli_error($con));
-    $rows_fetched=mysqli_num_rows($user_authentication_result);
-    if($rows_fetched==0){
-        //no user
-        //redirecting to same login page
-        ?>
-        <script>
-            window.alert("Wrong username or password");
-        </script>
-        <meta http-equiv="refresh" content="1;url=login.php" />
-        <?php
-        //header('location: login');
-        //echo "Wrong email or password.";
-    }else{
-        $row=mysqli_fetch_array($user_authentication_result);
-        $_SESSION['email']=$email;
-        $_SESSION['id']=$row['id'];  //user id
-        header('location: products.php');
-    }
-    
- ?>
+require 'connection.php';
+session_start();
+
+$email = $_POST['email'];
+$regex_email = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$/";
+if (!preg_match($regex_email, $email)) {
+    echo "Incorrect email. Redirecting you back to login page...";
+    ?>
+    <meta http-equiv="refresh" content="2;url=login.php" />
+    <?php
+    exit(); // Stop execution after redirection
+}
+
+$password = md5(md5($_POST['password']));
+if (strlen($password) < 6) {
+    echo "Password should have at least 6 characters. Redirecting you back to login page...";
+    ?>
+    <meta http-equiv="refresh" content="2;url=login.php" />
+    <?php
+    exit(); // Stop execution after redirection
+}
+
+$user_authentication_query = "SELECT id, email FROM users WHERE email = :email AND password = :password";
+$user_authentication_stmt = $con->prepare($user_authentication_query);
+$user_authentication_stmt->bindParam(':email', $email);
+$user_authentication_stmt->bindParam(':password', $password);
+$user_authentication_stmt->execute();
+
+$rows_fetched = $user_authentication_stmt->rowCount();
+if ($rows_fetched == 0) {
+    ?>
+    <script>
+        window.alert("Wrong username or password");
+    </script>
+    <meta http-equiv="refresh" content="1;url=login.php" />
+    <?php
+} else {
+    $row = $user_authentication_stmt->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['email'] = $email;
+    $_SESSION['id'] = $row['id'];  //user id
+    header('location: products.php');
+}
+?>
