885d0a6e15bc83089ab39ed4af86548aeceb4932
Andrew <oldm8reggie@gmail.com>
2023-08-30 15:05:21 +0200

    mysqli removal some more

--- LifestyleStore/check_if_added.php
+++ LifestyleStore/check_if_added.php
@@ -1,13 +1,19 @@
 <?php
-    
-    function check_if_added_to_cart($item_id){
-        //session_start();    
-        require 'connection.php';
-        $user_id=$_SESSION['id'];
-        $product_check_query="select * from users_items where item_id='$item_id' and user_id='$user_id' and status='Added to cart'";
-        $product_check_result=mysqli_query($con,$product_check_query) or die(mysqli_error($con));
-        $num_rows=mysqli_num_rows($product_check_result);
-        if($num_rows>=1)return 1;
-        return 0;
+function check_if_added_to_cart($item_id){
+    //session_start();
+    require 'connection.php';
+    $user_id = $_SESSION['id'];
+
+    $product_check_query = "SELECT * FROM users_items WHERE item_id = :item_id AND user_id = :user_id AND status = 'Added to cart'";
+    $product_check_stmt = $con->prepare($product_check_query);
+    $product_check_stmt->bindParam(':item_id', $item_id);
+    $product_check_stmt->bindParam(':user_id', $user_id);
+    $product_check_stmt->execute();
+
+    $num_rows = $product_check_stmt->rowCount();
+    if ($num_rows >= 1) {
+        return 1;
     }
+    return 0;
+}
 ?>
